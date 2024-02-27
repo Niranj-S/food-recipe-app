c01ed47cd84ebda9edaf903d9cb4a959d834ade5
Niranj-S <138951774+Niranj-S@users.noreply.github.com>
2024-02-28 00:43:25 +0530

    recipe.py

--- recipe.py
+++ recipe.py
@@ -1,11 +1,10 @@
-from dotenv import load_dotenv
-load_dotenv()
+
 import streamlit as st
 import os
 import google.generativeai as genai
 from PIL import Image
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets['API_KEY'])
 
 
 
